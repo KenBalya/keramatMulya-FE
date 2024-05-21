4e85ea07ab4a1f40fea876d913d6bf90187ed35c
KenBalya <124903836+KenBalya@users.noreply.github.com>
2024-05-21 23:12:15 +0700

    update components

--- app/components/HomeHero.tsx
+++ app/components/HomeHero.tsx
@@ -4,16 +4,16 @@ import React from 'react';
 const HomeHero = () => {
     return (
         <div className="relative pt-16 pb-32 flex content-center items-center justify-center"
-            style={{ minHeight: "75vh", backgroundImage: "url('/tekstil.jpg')", backgroundSize: "cover", backgroundPosition: "center" }}>
+            style={{ minHeight: "75vh", backgroundImage: "url('/batik.webp')", backgroundSize: "cover", backgroundPosition: "center" }}>
 
             <div className="container relative mx-auto">
                 <div className="items-center flex flex-wrap">
                     <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
-                        <div className="md:pr-12 ">
+                        <div className="md:pr-12 bg-slate-800 bg-opacity-90">
                             <h1 className="text-white font-semibold md:text-5xl text-2xl">
                                 Desa Kramat Mulya
                             </h1>
-                            <p className="mt-4 md:text-lg text-sm font-semibold">
+                            <p className="mt-4 md:text-lg text-sm font-semibold text-white">
                                 Desa Kramat Mulya, memiliki kampung yang terkenal dengan sebutan &quot;kampung gamis&quot;. Desa ini terkenal karena menjadi pusat produksi gamis, tekstil, kain yang berkualitas tinggi dan terkenal sampai mancanegara.
                             </p>
                         </div>
