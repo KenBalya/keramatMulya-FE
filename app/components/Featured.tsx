75756173a4f9f85976b743b1a50ac7d6baf59617
KenBalya <124903836+KenBalya@users.noreply.github.com>
2024-05-21 23:14:50 +0700

    update components

--- app/components/Featured.tsx
+++ app/components/Featured.tsx
@@ -3,12 +3,12 @@ import { FeatureCard } from './FeatureCard'
 
 export const Featured: React.FC = () => {
   return (
-    <section className="py-20 max-w-screen-xl container gap-3 sm:gap-7 md:gap-10 flex flex-col items-center relative z-20">
+    <section className="py-20 max-w-screen-xl container gap-3 sm:gap-7 md:gap-10 flex flex-col items-center relative z-20 text-white">
       <h2 className="text-center font-Poppins text-4xl sm:text-5xl md:text-6xl font-bold">
         Prestasi Kami
       </h2>
       <div className="bg-white/20 backdrop-blur-lg rounded-2xl px-4 sm:px-8 md:px-12 py-3 sm:py-6 md:py-9 border-2 border-white/20 text-justify">
-        <span className="text-center text-base sm:text-xl lg:text-2xl font-semibold">
+        <span className="text-center text-base sm:text-xl lg:text-2xl font-semibold text-white">
           “Desa Karamat Mulya adalah desa penghasil kerajinan tekstil terbaik di dunia. Kerajinan tekstil dari desa kami pernah diimpor sampe planet Mars.
           Desa kami juga merupakan desa yang senang bercocok tanam, ya bercocok tanam dalam konteks hubungan manusia.”
         </span>
